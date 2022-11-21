2b7e8eb1491feda412d721a5f3bcaf37e27bd3e2
Kwon HyukJin <117734137+3437k@users.noreply.github.com>
2022-11-22 00:01:18 +0900

    Update kotlin.kt

--- kotlin.kt
+++ kotlin.kt
@@ -1,4 +1,20 @@
+import kotlin.math.abs                                           
+
 fun main() {
 	val msg = "2022-11-21"
-	println("Kotlin, $msg")
+	println("Kotlin, $msg") // sout or soutv
+	
+	println(abs(-123)) // 123                
+	
+	println(Buyer().name) // NoName                              
+	println(Buyer().price) // 0                                  
+	println(Buyer().toString()) // Buyer(name='NoName', price=0) 
+	println(Buyer()) // Buyer(name='NoName', price=0)            
 }
+
+class Buyer(val name: String = "NoName", val price: Int = 0) {   
+                                                                 
+	override fun toString(): String {                            
+		return "Buyer(name='$name', price=$price)"               
+	}                                                            
+}                                                                
