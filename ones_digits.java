4ef8fc50c40a12218152ebcb574771a154015263
Kwon HyukJin <117734137+3437k@users.noreply.github.com>
2022-11-22 16:19:58 +0900

    Update ones_digits.java

--- ones_digits.java
+++ ones_digits.java
@@ -10,3 +10,23 @@ int n = 123;
 int ones = n % 10;
 int rest = n / 10;
 System.out.println(n + " -> " + rest + ", " + ones); // 123 -> 12, 3
+
+
+// 2022-11-22
+public class Main {
+  public static void main(String[] args) {
+      int n = 12345;
+      // n = 121 // Palindrome
+
+      int r = 0;
+      int result = 0;
+
+      while(n > 0) {
+          r = n % 10;
+          result = result * 10 + r;
+          n = n / 10;
+      }
+
+      System.out.println(result); // 54321
+  }
+}
