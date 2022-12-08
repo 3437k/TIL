c6362d3766cead8b2e42f9b1b4ee9874e7980baa
Kwon HyukJin <117734137+3437k@users.noreply.github.com>
2022-12-08 10:47:23 +0900

    Update java_test.java

--- java_test.java
+++ java_test.java
@@ -1,1 +1,9 @@
 // 2022-12-08
+
+// Caused by: java.time.format.DateTimeParseException:
+// Text '2022.12' could not be parsed: Unable to obtain LocalDate from TemporalAccessor:
+// {Year=2022, MonthOfYear=12},ISO of type java.time.format.Parsed
+
+String str = "2022.12";
+LocalDate parse = LocalDate.parse(str, DateTimeFormatter.ofPattern("yyyy.MM"));
+System.out.println("parse = " + parse);
